f83d0008c32b8c0f14625aee87af7d2954f53c98
Cleonildo Jr <cleu.junior@gmail.com>
2023-05-12 20:44:10 -0300

    Singleton

---
+++ src/main/java/com/br/study/patterns/creational/singleton/Connection.java
@@ -0,0 +1,6 @@
+package com.br.study.patterns.creational.singleton;
+
+public interface Connection {
+
+    void executeQuery(String query);
+}
---
+++ src/main/java/com/br/study/patterns/creational/singleton/Mysql.java
@@ -0,0 +1,50 @@
+package com.br.study.patterns.creational.singleton;
+
+public class Mysql implements Connection {
+    private static Mysql instance = null;
+    private String host;
+    private String user;
+    private String password;
+
+    private Mysql() {
+        this.host = "host";
+        this.user = "user";
+        this.password = "password";
+    }
+
+    public String getHost() {
+        return this.host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getUser() {
+        return this.user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public void executeQuery(String query) {
+    }
+
+    public static Mysql getInstance() {
+        if(instance == null) {
+            instance = new Mysql();
+        }
+
+        return instance;
+    }
+}
