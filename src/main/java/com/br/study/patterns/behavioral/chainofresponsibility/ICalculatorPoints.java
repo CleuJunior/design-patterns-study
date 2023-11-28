02d4f8e0eeac8e680865e0c06bb8ba1563f9f604
CleuJunior <cleu.junior@gmail.com>
2023-11-28 16:43:52 -0300

    Chain of Responsibility

--- src/main/java/com/br/study/patterns/behavioral/chainofresponsibility/ICalculatorPoints.java
+++
@@ -1,7 +0,0 @@
-package com.br.study.patterns.behavioral.chainofresponsibility;
-
-public interface ICalculatorPoints {
-
-    int calculatePoint(Order order);
-    void setNext(ICalculatorPoints next);
-}
