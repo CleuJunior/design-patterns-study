6f25ea4d225b5993c83c4a7cbf78f00c7e1a26a1
CleuJunior <cleu.junior@gmail.com>
2023-11-30 17:42:42 -0300

    Prototype

--- src/main/java/com/br/study/patterns/structural/facade/Consumer.java
+++
@@ -1,4 +0,0 @@
-package com.br.study.patterns.structural.facade;
-
-public record Consumer(String name, String cpf, String email) {
-}
--- src/main/java/com/br/study/patterns/structural/facade/Product.java
+++
@@ -1,6 +0,0 @@
-package com.br.study.patterns.structural.facade;
-
-import java.math.BigDecimal;
-
-public record Product(String name, String description, BigDecimal value) {
-}
